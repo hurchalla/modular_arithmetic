f8f224c69dffb4b1ea8c7d6e724e906b2184f1bc
Jeff Hurchalla <hurchalla@yahoo.com>
2018-08-11 15:48:05 -0700

    improve comments

--- include/hurchalla/modular_arithmetic/modular_addition.h
+++ include/hurchalla/modular_arithmetic/modular_addition.h
@@ -12,15 +12,15 @@ namespace hurchalla { namespace modular_arithmetic {
 template <typename T>
 T modular_addition_prereduced_inputs(T a, T b, T modulus)
 {
-	precondition_static(std::is_unsigned<T>::value);  //T unsigned integral type
-	precondition(modulus>0);
-	precondition(a<modulus);
-	precondition(b<modulus);
-    // Postconditions:
+    precondition_static(std::is_unsigned<T>::value);  //T unsigned integral type
+    precondition(modulus>0);
+    precondition(a<modulus);    // i.e. the input must be prereduced
+    precondition(b<modulus);    // i.e. the input must be prereduced
+    // Postcondition:
     //   Returns (a+b)%modulus.  Guarantees no overflow internally on a+b.
-
+    
     /* We want essentially-  result = (a+b < modulus) ? a+b : a+b-modulus
-       But due to overflow potential we need to write it as below */
+       But due to potential overflow on a+b we need to write it as follows */
     T tmp = modulus - b;
     T result = (a < tmp) ? a+b : a-tmp;
     return result;
