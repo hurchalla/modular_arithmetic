1eecbf44ba89001b2983fa43e477bc3f244ebf4d
Jeff Hurchalla <hurchalla@yahoo.com>
2018-08-16 06:26:13 -0700

    add modular_pow

---
+++ include/hurchalla/modular_arithmetic/modular_pow.h
@@ -0,0 +1,27 @@
+
+#ifndef HURCHALLA_MODULAR_ARITHMETIC_MODULAR_POW_H__INCLUDED
+#define HURCHALLA_MODULAR_ARITHMETIC_MODULAR_POW_H__INCLUDED
+
+
+#include "hurchalla/modular_arithmetic/internal/impl_modular_pow.h"
+#include "hurchalla/programming_by_contract/programming_by_contract.h"
+#include <type_traits>
+
+namespace hurchalla { namespace modular_arithmetic {
+
+
+template <typename T>
+T modular_pow(T base, T exponent, T modulus)
+{
+    precondition_static(std::is_unsigned<T>::value);  //T unsigned integral type
+    precondition(modulus > 1);
+    // Postcondition:
+    //   Returns the modular exponentiation of base^exponent (mod modulus).
+
+    return impl_modular_pow(base, exponent, modulus);
+}
+
+
+}}  // end namespace
+
+#endif
