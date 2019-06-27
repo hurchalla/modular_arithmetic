f2ca3779658b03833541fcea0f71f8e506a8541b
Jeff Hurchalla <hurchalla@yahoo.com>
2019-06-27 15:37:18 -0700

    initial commit

---
+++ include/hurchalla/modular_arithmetic/modular_multiplicative_inverse.h
@@ -0,0 +1,37 @@
+
+#ifndef HURCHALLA_MODULAR_ARITHMETIC_MODULAR_MULTIPLICATIVE_INVERSE_H__INCLUDED
+#define HURCHALLA_MODULAR_ARITHMETIC_MODULAR_MULTIPLICATIVE_INVERSE_H__INCLUDED
+
+
+#include "hurchalla/modular_arithmetic/internal/impl_modular_multiplicative_inverse.h"
+#include "hurchalla/modular_arithmetic/modular_multiplication.h"
+#include "hurchalla/programming_by_contract/programming_by_contract.h"
+#include <type_traits>
+
+namespace hurchalla { namespace modular_arithmetic {
+
+
+// Note: Calling with a < modulus slightly improves performance.
+// [The multiplicative inverse is an integer > 0 and < modulus, such that
+//    a * multiplicative_inverse == 1 (mod modulus).   It is a unique number,
+//    but it exists if and only if 'a' and 'modulus' are coprime.]
+template <typename T>
+T modular_multiplicative_inverse(T a, T modulus)
+{
+    precondition_static(std::is_unsigned<T>::value);  //T unsigned integral type
+    precondition(modulus>1);
+
+    T inverse = impl_modular_multiplicative_inverse(a, modulus);
+
+    postcondition(inverse < modulus);
+    //Postcondition: Returns 0 if the inverse does not exist. Otherwise returns
+    //   the value of the inverse (which is never 0, given that modulus>1).
+    postcondition(inverse == 0 || modular_multiplication_prereduced_inputs(
+                                           a % modulus, inverse, modulus) == 1);
+    return inverse;
+}
+
+
+}}	// end namespace
+
+#endif
