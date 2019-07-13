8b6dc24255bae28d7b884ea5b6f3c8bf46d2d280
Jeff Hurchalla <hurchalla@yahoo.com>
2019-07-13 01:41:40 -0700

    initial commit

---
+++ montgomery/include/hurchalla/modular_arithmetic/montgomery/internal/nonmontywrapper.h
@@ -0,0 +1,15 @@
+
+#ifndef HURCHALLA_MODULAR_ARITHMETIC_NON_MONTY_WRAPPER_H__INCLUDED
+#define HURCHALLA_MODULAR_ARITHMETIC_NON_MONTY_WRAPPER_H__INCLUDED
+
+
+namespace hurchalla { namespace modular_montgomery {
+
+
+template <typename T>
+struct NonMontyWrapper {};
+
+
+}} // end namespace
+
+#endif
