5c19d5200ca2b00eb24add3892603e9e930a8a6f
Jeff Hurchalla <hurchalla@yahoo.com>
2019-07-13 01:36:29 -0700

    initial commit

---
+++ montgomery/include/hurchalla/modular_arithmetic/montgomery/internal/montgomeryvalue.h
@@ -0,0 +1,32 @@
+
+#ifndef HURCHALLA_MODULAR_MONTGOMERY_MONTGOMERY_VALUE_H__INCLUDED
+#define HURCHALLA_MODULAR_MONTGOMERY_MONTGOMERY_VALUE_H__INCLUDED
+
+namespace hurchalla { namespace modular_montgomery {
+
+
+// A simple wrapper for T, used to designate that a value is in Montgomery Form
+template<typename T> struct MontgomeryValue final {
+public:
+    MontgomeryValue() noexcept {}; // This constructor purposely does not
+                                   // initialize 'value' - the contents are
+                                   // undefined until the object is assigned to.
+    explicit MontgomeryValue(T val) noexcept : value(val) {}
+    T get() noexcept { return value; }
+private:
+    T value;
+};
+
+template<typename T>
+bool operator==(MontgomeryValue<T>& lhs, MontgomeryValue<T>& rhs) {
+    return lhs.get() == rhs.get();
+}
+template<typename T>
+bool operator!=(MontgomeryValue<T>& lhs, MontgomeryValue<T>& rhs) {
+    return !(lhs == rhs);
+}
+
+
+}} // end namespace
+
+#endif
