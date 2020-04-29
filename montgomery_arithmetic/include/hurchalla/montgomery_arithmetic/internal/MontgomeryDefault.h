f5f8f4607262d0059d63fe755276c5acff210126
Jeffrey Hurchalla <hurchalla@yahoo.com>
2020-04-29 11:03:43 -0700

    simplify MontgomeryDefault template

--- montgomery_arithmetic/include/hurchalla/montgomery_arithmetic/internal/MontgomeryDefault.h
+++ montgomery_arithmetic/include/hurchalla/montgomery_arithmetic/internal/MontgomeryDefault.h
@@ -13,11 +13,7 @@
 namespace hurchalla { namespace montgomery_arithmetic {
 
 
-// If this primary template is instantiated, then T is not known to
-// std::type_traits.  In this case we require that T is an unsigned integral
-// type.  (Signed integral type for T is not possible because we would need to
-// derive an unsigned integral type from it, and std::make_unsigned won't work
-// for any type unknown to std::type_traits.)
+// If this primary template is instantiated, then T is an unsigned integral type
 template <typename T, typename Enable = void>
 class MontgomeryDefault {
     static_assert(std::numeric_limits<T>::is_integer, "");
@@ -26,32 +22,28 @@ class MontgomeryDefault {
 public:
     using type =
         typename std::conditional<
-            !std::is_same<typename sized_uint<ubits*2>::type, void>::value
-              && ubits*2 <= TARGET_BIT_WIDTH,
+            !(std::is_same<typename sized_uint<ubits*2>::type, void>::value)
+                && (ubits*2 <= TARGET_BIT_WIDTH),
             MontySqrtRange<typename sized_uint<ubits*2>::type>,
             MontyFullRange<T>
         >::type;
 };
 
-// If this partial specialization is instantiated, then T is an integral type
-// known to std::type_traits (the class needs to safely use std::make_unsigned).
+// If this partial specialization is instantiated, then T is a signed integral
+// type known to std::type_traits (this class needs to be able to safely use
+// std::make_unsigned)
 template <typename T>
-class MontgomeryDefault<T,
-                    typename std::enable_if<std::is_integral<T>::value>::type> {
-    static_assert(std::is_integral<T>::value, "");
+class MontgomeryDefault<T, typename
+std::enable_if<std::is_integral<T>::value && std::is_signed<T>::value>::type> {
     using U = typename std::make_unsigned<T>::type;
     static constexpr int ubits = std::numeric_limits<U>::digits;
 public:
     using type =
         typename std::conditional<
-            !std::is_same<typename sized_uint<ubits*2>::type, void>::value
-              && ubits*2 <= TARGET_BIT_WIDTH,
+            !(std::is_same<typename sized_uint<ubits*2>::type, void>::value)
+                && (ubits*2 <= TARGET_BIT_WIDTH),
             MontySqrtRange<typename sized_uint<ubits*2>::type>,
-            typename std::conditional<
-                std::is_signed<T>::value,
-                MontyHalfRange<U>,
-                MontyFullRange<U>
-            >::type
+            MontyHalfRange<U>
         >::type;
 };
 
