9d5010453315d9101793b77d6c54436c826051b3
Jeffrey Hurchalla <hurchalla@yahoo.com>
2020-09-14 11:36:38 -0700

    add (plain) inverse_mod_r.  relax convertIn preconditions to allow any input >= 0

---
+++ montgomery_arithmetic/include/hurchalla/montgomery_arithmetic/detail/inverse_mod_r.h
@@ -0,0 +1,123 @@
+// --- This file is distributed under the MIT Open Source License, as detailed
+// by the file "LICENSE.TXT" in the root of this repository ---
+
+#ifndef HURCHALLA_MONTGOMERY_ARITHMETIC_INVERSE_MODR_H_INCLUDED
+#define HURCHALLA_MONTGOMERY_ARITHMETIC_INVERSE_MODR_H_INCLUDED
+
+
+#include "hurchalla/montgomery_arithmetic/detail/sized_uint.h"
+#include "hurchalla/montgomery_arithmetic/detail/safely_promote_unsigned.h"
+#include "hurchalla/modular_arithmetic/detail/ma_numeric_limits.h"
+#include "hurchalla/modular_arithmetic/detail/platform_specific/compiler_macros.h"
+#include "hurchalla/programming_by_contract/programming_by_contract.h"
+#include <type_traits>
+
+namespace hurchalla { namespace montgomery_arithmetic {
+
+
+// For discussion purposes, let R = 2^(ma_numeric_limits<T>::digits).  For
+// example if T is uint64_t, then R = 2^64.
+
+
+namespace detail_imr {
+    template <int n>
+    constexpr int log2()
+    {
+      // PRECONDITION: n!=0 (this isn't possible to express via static_assert)
+      static_assert(n>=0, "");
+      static_assert(n==1 || (n/2)*2 == n, "");
+      return (n<=1) ? 0 : 1 + log2<n/2>();
+    }
+    #ifndef HURCHALLA_TARGET_BIT_WIDTH
+    #error "HURCHALLA_TARGET_BIT_WIDTH must be defined"
+    #endif
+
+    // This is the generalized Dumas algorithm for the inverse (mod R).
+    // TODO:
+    // I haven't yet published my generalized form of the Dumas algorithm, but
+    // the Dumas algorithm comes from  https://arxiv.org/abs/1209.6626
+    // The closest information available at the moment is from Marc Reynolds at
+    // http://marc-b-reynolds.github.io/math/2017/09/18/ModInverse.html
+    // However, Reynolds presents a straightforward adaptation of Dumas's
+    // algorithm.  This generalized form is a slightly different algo.
+    //
+    // Note: Dumas's alg only makes sense to use for the native integral types -
+    // Newton's method becomes more efficient when larger types are required.
+    template <typename T, int bits>
+    HURCHALLA_FORCE_INLINE
+    typename std::enable_if<bits<=HURCHALLA_TARGET_BIT_WIDTH, T>::type
+    impl_inverse(T a)
+    {
+        namespace ma = hurchalla::modular_arithmetic;
+        static_assert(bits == ma::ma_numeric_limits<T>::digits, "");
+        static_assert(std::is_unsigned<T>::value, ""); //T native unsign integer
+        HPBC_PRECONDITION2(a % 2 == 1);
+
+        // avoid undefined behavior that could result if T is an unsigned type
+        // that would be promoted to (signed) 'int'.
+        using U = typename safely_promote_unsigned<T>::type;
+        U b = static_cast<U>(a);
+
+        U x = (3*b)^2;  // good to 5 bits, but we'll treat it as good to only 4
+        static const constexpr int goodbits = 4;  // must be a power of 2
+        U s = b*x;
+        U y = 1-s;
+  
+        static_assert((bits/goodbits)*goodbits == bits, "");
+        static const constexpr int iterations = log2<bits/goodbits>();
+        HURCHALLA_REQUEST_UNROLL_LOOP
+        for (int i=0; i<iterations; ++i) {
+            U t = y+1;
+            y = y*y;
+            x = x*t;
+        }
+        return static_cast<T>(x);
+    }
+
+    // This is Newton's method algorithm for the inverse (mod R).
+    // To get the starting bits of 'x' we recurse until we use Dumas's method
+    // (it's more efficient than Newton's method for native integer types).
+    template <typename T, int bits>
+    HURCHALLA_FORCE_INLINE
+    typename std::enable_if<!(bits<=HURCHALLA_TARGET_BIT_WIDTH), T>::type
+    impl_inverse(T a)
+    {
+        static_assert((bits/2)*2 == bits, "");
+        using T2 = typename sized_uint<bits/2>::type;
+        using T3 = typename std::conditional<!(std::is_same<T2,void>::value),
+                                         T2, T>::type;
+        HPBC_PRECONDITION2(a % 2 == 1);
+
+        // set x so that the lower ('bits'/2) half of the bits are good.
+        T x = static_cast<T>(impl_inverse<T3, bits/2>(static_cast<T3>(a)));
+  
+        // use one step of the standard newtons method algorithm for the inverse
+        // to double the number of good bits.
+        return x*(static_cast<T>(2) - a*x);
+    }
+}
+
+
+// Returns the integer x satisfying  x*a ≡ 1 (mod R)
+template <typename T>
+T inverse_mod_r(T a)
+{
+    namespace ma = hurchalla::modular_arithmetic;
+    static_assert(ma::ma_numeric_limits<T>::is_integer, "");
+    static_assert(!(ma::ma_numeric_limits<T>::is_signed), "");
+    static_assert(ma::ma_numeric_limits<T>::is_modulo, "");
+    HPBC_PRECONDITION2(a % 2 == 1);
+
+    T inv = detail_imr::impl_inverse<T, ma::ma_numeric_limits<T>::digits>(a);
+
+    // guarantee inv*a ≡ 1 (mod R)
+    using U = typename safely_promote_unsigned<T>::type;
+    HPBC_POSTCONDITION2(static_cast<T>(static_cast<U>(inv) * static_cast<U>(a))
+                        == static_cast<T>(1));
+    return inv;
+}
+
+
+}} // end namespace
+
+#endif
---
+++ test/montgomery_arithmetic/test_inverse_mod_r.cpp
@@ -0,0 +1,77 @@
+// --- This file is distributed under the MIT Open Source License, as detailed
+// by the file "LICENSE.TXT" in the root of this repository ---
+
+
+#include "hurchalla/montgomery_arithmetic/detail/inverse_mod_r.h"
+#include "hurchalla/montgomery_arithmetic/detail/safely_promote_unsigned.h"
+#include "hurchalla/modular_arithmetic/detail/ma_numeric_limits.h"
+#include "gtest/gtest.h"
+#include <cstdint>
+
+
+template <typename T>
+void test_single_inverse(T a)
+{
+    namespace mont = hurchalla::montgomery_arithmetic;
+    using U = typename mont::safely_promote_unsigned<T>::type;
+    T one = static_cast<T>(1);
+
+    T inv = mont::inverse_mod_r(a);
+    EXPECT_TRUE(static_cast<T>(static_cast<U>(inv) * static_cast<U>(a)) == one);
+}
+
+
+template <typename T>
+void test_inverse_exhaustive()
+{
+    namespace ma = hurchalla::modular_arithmetic;
+    T tmax = ma::ma_numeric_limits<T>::max();
+    T evenmax = static_cast<T>((tmax/2)*2);
+    T oddmax = (evenmax != tmax) ? tmax : static_cast<T>(tmax - 1);
+
+    for (T a=oddmax; a>1; a=static_cast<T>(a-2))
+        test_single_inverse(a);
+    test_single_inverse(static_cast<T>(1));
+}
+
+
+template <typename T>
+void test_inverse_mod_r()
+{
+    namespace ma = hurchalla::modular_arithmetic;
+    T tmax = ma::ma_numeric_limits<T>::max();
+    T evenmax = static_cast<T>((tmax/2)*2);
+    T oddmax = (evenmax != tmax) ? tmax : static_cast<T>(tmax - 1);
+    T oddhalfmax = static_cast<T>((tmax/4)*2 + 1);
+
+    // inverse_mod_r's preconditions require input a is odd.
+
+    test_single_inverse(static_cast<T>(1));
+    test_single_inverse(static_cast<T>(3));
+    test_single_inverse(static_cast<T>(5));
+    test_single_inverse(static_cast<T>(7));
+
+    test_single_inverse(static_cast<T>(oddmax));
+    test_single_inverse(static_cast<T>(oddmax - 2));
+    test_single_inverse(static_cast<T>(oddmax - 4));
+
+    test_single_inverse(static_cast<T>(oddhalfmax));
+    test_single_inverse(static_cast<T>(oddhalfmax + 2));
+    test_single_inverse(static_cast<T>(oddhalfmax - 2));
+}
+
+
+namespace {
+    TEST(MontgomeryArithmetic, inverse_mod_r) {
+        test_inverse_mod_r<std::uint8_t>();
+        test_inverse_mod_r<std::uint16_t>();
+        test_inverse_mod_r<std::uint32_t>();
+        test_inverse_mod_r<std::uint64_t>();
+#if HURCHALLA_COMPILER_HAS_UINT128_T()
+        test_inverse_mod_r<__uint128_t>();
+#endif
+
+        test_inverse_exhaustive<std::uint8_t>();
+        test_inverse_exhaustive<std::uint16_t>();
+    }
+}
